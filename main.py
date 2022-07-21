e57ad41dabe87a9bea1f342eda816b84e061bc57
James Ervin <ervinhfamily@gmail.com>
2022-07-21 11:26:33 -0500

    Added placeholder/guiding comments.

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-#sstv -d NASASEES.wav -o result.png
+#sstv -d NASASEES.wav -o result.png'
+
 import os
-os.system('cmd /k "sstv -d NASASEES.wav -o result.png"')
+
+def sampleNoiseLevel():
+    #Get Noise Level
+    pass
+
+def monitorForIncrease():
+    #Listen until a significant increase in noise level, then begin recording audio snippets.
+    pass
+
+def recordSegments():
+    #36 Second Transmissions, 0.25 Second break in-between.
+    #Currently: 1 SSTV Picture, 5 Color Pictures per Data Transmission.
+    pass
+
+def processSegments():
+    #Convert .wav files to .png files.
+    pass
+
+#os.system('cmd /k "sstv -d NASASEES.wav -o result.png"')

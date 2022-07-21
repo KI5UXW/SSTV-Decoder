1981c68036327dfe7f3177d0d4fe0e17ecdc2bb8
James Ervin <ervinhfamily@gmail.com>
2022-07-21 14:04:26 -0500

    Update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #sstv -d NASASEES.wav -o result.png'
 
 import os
+import sounddevice as sd
+from scipy.io.wavfile import write
 
 def sampleNoiseLevel():
     #Get Noise Level
@@ -13,10 +15,24 @@ def monitorForIncrease():
 def recordSegments():
     #36 Second Transmissions, 0.25 Second break in-between.
     #Currently: 1 SSTV Picture, 5 Color Pictures per Data Transmission.
-    pass
+    #Approximately a 3' 12" long transmission for current length.
+    fs = 44100  # Sample rate
+    seconds = (3*60) + 12 # Duration of recording
+
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
+    write('RecordedAudio.wav', fs, myrecording)  # Save as WAV file 
+
+def separateSegments()
 
 def processSegments():
     #Convert .wav files to .png files.
     pass
 
 #os.system('cmd /k "sstv -d NASASEES.wav -o result.png"')
+
+sampleNoiseLevel()
+monitorForIncrease()
+recordSegments()
+separateSegments()
+processSegments()
